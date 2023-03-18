751d4c28bae558870725d85d9eb1c391cb57059e
aland <aandreenko1997@gmail.com>
2023-03-18 22:11:54 +0700

    config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   STEP: 15,
   SERVER_PORT: 443,
   USE_HTTPS: true,
-  DEV_MODE: false,
+  DEV_MODE: true,
   MONGODB_CONNECT_STR: 'mongodb://127.0.0.1:27017/test',
   HOST: 'https://aland97.ru'
 }
