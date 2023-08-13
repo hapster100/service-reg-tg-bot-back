354a88dd13259ee60d93caa81f5a32b70d982a7e
aland <aandreenko1997@gmail.com>
2023-08-13 14:10:19 +0700

    conf

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
+const isDev = !!process.env.DEV;
+
 module.exports = {
   STEP: 15,
-  SERVER_PORT: 443,
-  USE_HTTPS: true,
-  DEV_MODE: !!process.env.DEV,
-  MONGODB_CONNECT_STR: process.env.DEV ? 'mongodb://127.0.0.1:27017/test' : 'mongodb://bot:ds1ms9dfsmaze9crop7@127.0.0.1:27017/shedullebot',
-  HOST: process.env.DEV ? 'https://aland97.ru' : 'https://barberbot96.ru'
+  SERVER_PORT: isDev ? 3000 : 443,
+  USE_HTTPS: !isDev,
+  DEV_MODE: isDev,
+  MONGODB_CONNECT_STR: isDev ? 'mongodb://127.0.0.1:27017/test' : 'mongodb://prod',
+  HOST: isDev ? 'http://localhost:3000' : 'https://barberbot96.ru'
 }
